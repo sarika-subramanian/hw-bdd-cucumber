e232dea0f431ab612d799d0105fd865e88fb2b41
EC2 Default User <ec2-user@ip-172-31-90-66.ec2.internal>
2020-04-26 14:45:46 +0000

    hw 3 done

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -25,7 +25,13 @@ Background: movies have been added to database
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
   # your steps here
+  Then I should see "Aladdin" before "Amelie"
+  And I should see "Chicken Run" before "Chocolat"
+  And I should see "Raiders of the Lost Ark" before "The Terminator"
 
 Scenario: sort movies in increasing order of release date
   When I follow "Release Date"
   # your steps here
+  Then I should see "2001: A Space Odyssey" before "Raiders of the Lost Ark"
+  And I should see "Chocolat" before "Amelie"
+  And I should see "The Incredibles" before "The Help"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,6 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
   fail "Unimplemented"
 end
@@ -18,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.index(e1) < page.body.index(e2)
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,9 +32,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   fail "Unimplemented"
+  rating_list.split(", ").each do |rating|
+    if uncheck
+      uncheck("ratings[#{rating}]")
+    else
+     check("ratings[#{rating}]")
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  page.all('table tr').count.should == Movie.count + 1
 end
