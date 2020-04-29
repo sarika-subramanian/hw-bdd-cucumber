ed958cb49990aefb1a47eb2e06a9ad0b0ffff029
EC2 Default User <ec2-user@ip-172-31-90-66.ec2.internal>
2020-04-29 20:27:54 +0000

    fixed hw 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -6,7 +6,7 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -31,12 +31,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #fail "Unimplemented"
   rating_list.split(", ").each do |rating|
     if uncheck
       uncheck("ratings[#{rating}]")
     else
      check("ratings[#{rating}]")
+   end 
+ end 
+
 end
 
 Then /I should see all the movies/ do
